9757537f8c101bf7477fbe6b5abba55c1bc4bd6a
S6a8mMaz <160770668+S6a8mMaz@users.noreply.github.com>
2026-04-25 18:19:43 +0200

    Correzione CreaDB.py
    Frase scritta in inglese

--- DB/CreaDB.py
+++ DB/CreaDB.py
@@ -9,6 +9,4 @@ def CreaDB(): # Crea fisicamente tutte le tabelle definite nei modelli ORM
 if __name__ == "__main__": # Esegue l'inizializzazione del database se il file è lanciato direttamente
     CreaDB()
 
-    print("\n\t*** Database e tabelle creati correttamente ***\n")
-
-# Per creare il Database e le tabelle vuote --> python -m DB.CreaDB
+    print("\n\t*** Database and tables created ***\n")
